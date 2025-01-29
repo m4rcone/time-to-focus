0775a97ed501c23fbad8862732e4502db2f56aec
m4cone <boffmarcone@gmail.com>
2025-01-29 14:53:31 -0300

    add `header` component

---
+++ app/ui/components/header.tsx
@@ -0,0 +1,10 @@
+import { Focus } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-center gap-2 p-4 text-center text-xl">
+      Time To Focus
+      <Focus />
+    </header>
+  );
+}
