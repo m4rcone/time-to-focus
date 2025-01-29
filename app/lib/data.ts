7d24b26618db3572e63cd63d47a3567719151c37
m4cone <boffmarcone@gmail.com>
2025-01-29 14:50:56 -0300

    add `data.ts` file

---
+++ app/lib/data.ts
@@ -0,0 +1,18 @@
+export const audios = [
+  {
+    name: "FlameKindling",
+    url: "/assets/fireplace.wav",
+  },
+  {
+    name: "Leaf",
+    url: "/assets/forest.wav",
+  },
+  {
+    name: "CloudRain",
+    url: "/assets/rain.wav",
+  },
+  {
+    name: "Coffee",
+    url: "/assets/coffee.wav",
+  },
+];
